3545be0bb5854a6d61f23af92d474438c5bae4fc
Matt Moya <matiasmoya@gmail.com>
2014-06-09 23:26:16 -0300

    change fixture

--- www/js/fixture.js
+++ www/js/fixture.js
@@ -1,38 +1,47 @@
 function createFixture() {
 
 	var Grupo = [];
-	var option = document.getElementById("select");
+	/*var option = document.getElementById("select");
 	var option = option.value;
+	Grupo[0] = option;*/
+
+	for (var i = 1; i < 3; i++ ) {
+		option = "A" + i;
+		
 	
+		switch (option){
+			case "A1":
+				Grupo[1] = "Brasil";
+				Grupo[2] = "Croacia";
+				Grupo[3] = "México";
+				Grupo[4] = "Camerún";
+				break;
+			case "A2":
+				Grupo[1] = "asd";
+				Grupo[2] = "zxc";
+				Grupo[3] = "saco";
+				Grupo[4] = "wea";
+				break;
 
-	switch (option){
-		case "A":
-			Grupo[1] = "Brasil";
-			Grupo[2] = "Croacia";
-			Grupo[3] = "México";
-			Grupo[4] = "Camerún";
-			break;
-		case "B":
-			Grupo[1] = "asd";
-			Grupo[2] = "zxc";
-			Grupo[3] = "saco";
-			Grupo[4] = "wea";
-			break;
+		}
+		$('#fixture').append('<div class="A'+ i +'"></div>');
+		$('.A'+ i +'').append('<h2>Grupo ' + i + '</h2>')
+		$('#fixture').find('.A'+i).append(
+			'<div class="team partido' + i + '"><span>' + Grupo[1] + '</span> VS <span>' + Grupo[2] + '</span></div>',
+			'<div class="team partido' + i + '"><span>' + Grupo[3] + '</span> VS <span>' + Grupo[4] + '</span></div>',
+			'<div class="team partido' + i + '"><span>' + Grupo[1] + '</span> VS <span>' + Grupo[3] + '</span></div>',
+			'<div class="team partido' + i + '"><span>' + Grupo[2] + '</span> VS <span>' + Grupo[4] + '</span></div>',
+			'<div class="team partido' + i + '"><span>' + Grupo[4] + '</span> VS <span>' + Grupo[1] + '</span></div>',
+			'<div class="team partido' + i + '"><span>' + Grupo[2] + '</span> VS <span>' + Grupo[3] + '</span></div>'	
+			);
 
+		/*
+		
+		$('.partido1').append('<span>' + Grupo[1] + '</span> VS <span>' + Grupo[2] + '</span>');	
+		$('.partido2').append('<span>' + Grupo[3] + '</span> VS <span>' + Grupo[4] + '</span>');	
+		$('.partido3').append('<span>' + Grupo[1] + '</span> VS <span>' + Grupo[3] + '</span>');	
+		$('.partido4').append('<span>' + Grupo[2] + '</span> VS <span>' + Grupo[4] + '</span>');	
+		$('.partido5').append('<span>' + Grupo[4] + '</span> VS <span>' + Grupo[1] + '</span>');	
+		$('.partido6').append('<span>' + Grupo[2] + '</span> VS <span>' + Grupo[3] + '</span>');*/
 	}
-	$('.partido1').empty();
-	$('.partido2').empty();
-	$('.partido3').empty();
-	$('.partido4').empty();
-	$('.partido5').empty();
-	$('.partido6').empty();
-	
-	$('.partido1').append('<span>' + Grupo[1] + '</span> VS <span>' + Grupo[2] + '</span>');	
-	$('.partido2').append('<span>' + Grupo[3] + '</span> VS <span>' + Grupo[4] + '</span>');	
-	$('.partido3').append('<span>' + Grupo[1] + '</span> VS <span>' + Grupo[3] + '</span>');	
-	$('.partido4').append('<span>' + Grupo[2] + '</span> VS <span>' + Grupo[4] + '</span>');	
-	$('.partido5').append('<span>' + Grupo[4] + '</span> VS <span>' + Grupo[1] + '</span>');	
-	$('.partido6').append('<span>' + Grupo[2] + '</span> VS <span>' + Grupo[3] + '</span>');	
-	
-
 }
